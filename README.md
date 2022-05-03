58be997354d8cc1b68c048e40f05351ad8b58d11
cappob20 <cappob20@users.noreply.github.com>
2022-05-02 22:20:15 -0400

    Updated README.md and script.py

---
+++ README.md
@@ -0,0 +1,29 @@
+# Employee Database
+This is a postgresql database example script that allows the user to add, remove, and update employee data in command prompt (tested only in Windows 10). It also allows the user to view one employee information, or all of the employees information.
+
+This is designed as a project exercise for Grover Gaming's Software Engineer position.
+
+## Installation
+Install dependencies with `pip install`:
+* `psycopg2`
+
+## Before Running
+####Update `conn` to connect to your database
+`dbname=` Your database name
+`user=` Your username
+`host=` Your hostname (or localhost)
+`port=` Your port number
+`password=` Your password
+
+## Running
+Run `python script.py` from the root project directory to run the application.
+
+## Usage
+**Add:** Add an employee to the database. Must include employee name, job type, and date of hire (in the form of mm/dd/yyy)
+**Remove:** Remove an employee from the database.
+**Update:** Update an existing employee from the database. Able to change employee name, job type, and date of hire (in the form of mm/dd/yyy)
+**View All:** Shows all of the current employees from the database.
+**View One:** Shows one of the current employees from the database that is specified.
+
+
+<sup>by Brianna Cappo</sup>
--- script.py
+++ script.py
@@ -1,15 +1,22 @@
+# Created by Brianna Cappo
 import psycopg2
 import psycopg2.extras
 import sys
 from psycopg2.errors import InvalidDatetimeFormat
 
+# EDIT THIS BEFORE RUNNING
+# sets up connection with databse
 conn = psycopg2.connect("dbname='EmployeeDB' user='postgres' host='localhost' port='5433' password='password'")
 
+# get SQL cursor
 def getCursor():
     return conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
+# checks to see if there is a name that already exists
 def nameExists(name):
+    # with statment will automatically commit
     with conn:
+        # with statement will automatically close cursor
         with getCursor() as cur:
             cur.execute("SELECT * FROM employee WHERE name = %s;", (name,))
             if len(cur.fetchall()) == 0:
@@ -17,18 +24,19 @@ def nameExists(name):
             else:
                 return True
 
+# retrives the employee name from the databse
 def getName():
     name = input("Enter the name of Employee you want to update: ")
     while True:
         if nameExists(name):
             return name
         else:
-            print("No employee found with that name. ")
+            print("No employee found with that name.")
             name = input("Enter the name of Employee you want to update: ")
 
-
+# user can only type mm/dd/yyy for the date
 def inputDate(name):
-    doh = input("Enter the hire date: ")
+    doh = input("Enter the Date of hire: ")
     while True:
         try:
             with conn:
@@ -37,28 +45,35 @@ def inputDate(name):
             conn.close()
             break
         except InvalidDatetimeFormat as e:
-            print("Sorry, that is an invalid date. ")
-            doh = input("Enter the hire date: ")
+            print("Sorry, that is an invalid date.")
+            doh = input("Enter the Date of hire: ")
 
+# shows one of the current employees from the database that is specified
 def viewOne():
-    name = input("Enter the name of Employee you want to view: ")
+    name = input("Enter the name of employee you want to view: ")
     with conn:
         with getCursor() as cur:
             cur.execute("SELECT * FROM employee WHERE name = %s;", (name,))
-            print(cur.fetchone())
+            for employee in cur.fetchall():
+                if name == employee['name']:
+                    print(f"Name: {employee['name']} | Job Title: {employee['job_title']} | Date of hire: {employee['doh'].strftime('%m/%d/%Y')}")
 
+# shows all of the current employees from the database
 def viewAll():
     with conn:
         with getCursor() as cur:
             cur.execute("SELECT * FROM employee;")
-            print(cur.fetchall())
+            for employee in cur.fetchall():
+                print(f"Name: {employee['name']} | Job Title: {employee['job_title']} | Date of hire: {employee['doh'].strftime('%m/%d/%Y')}")
 
+# update an existing employee from the database
+# able to change employee name, job type, and date of hire (in the form of mm/dd/yyy)
 def updateEmployee():
     name = getName()
-    prompt = input("Please select an option to update: \n 1. Name of employee \n 2. Job Title \n 3. Date of hire \n")
+    prompt = input("Please select an option to update: \n 1. Employee Name \n 2. Job Title \n 3. Date of Hire \n")
 
     if (prompt == "1"):
-        update_name = input("Enter the new name for the Employee: ")
+        update_name = input("Enter the new employee name: ")
         with conn:
             with getCursor() as cur:
                 cur.execute(("UPDATE employee SET name = %s WHERE name = %s;"), (update_name, name,))
@@ -72,42 +87,48 @@ def updateEmployee():
     if (prompt == "3"):
         inputDate(name)
 
-    print("Record Updated successfully ")
+    print("Record Updated successfully!")
 
+# remove an employee from the database
 def removeEmployee():
     name = input("Enter the name of Employee you want to remove: ")
     with conn:
         with getCursor() as cur:
             cur.execute("DELETE FROM employee WHERE name = %s;", (name,))
 
-    print("Employee removed successfully ")
+    print("Employee removed successfully!")
 
+# add an employee to the database
+# must include employee name, job type, and date of hire (in the form of mm/dd/yyy)
 def addEmployee():
-    name = input("Enter a name: ")
+    name = input("Enter employee name: ")
     job_title = input("Enter the job title: ")
 
     # with statment will automatically commit
     with conn:
+        # with statement will automatically close cursor
         with getCursor() as cur:
             cur.execute("INSERT INTO employee (name) VALUES(%s)", (name,))
             cur.execute(("UPDATE employee SET job_title = %s WHERE name = %s;"), (job_title, name,))
     inputDate(name)
-    print("Employee added successfully ")
+    print("Employee added successfully!")
 
+# prompts user in command prompt to choose an option
 def promptUser():
-    prompt = input("Please select an option: \n 1. add \n 2. remove \n 3. update \n 4. view all \n 5. view one \n")
+    prompt = input("Please select an option: \n 1. Add Employee \n 2. Remove Employee \n 3. Update Existing Employee \n 4. View All Employees \n 5. View One Employee \n")
+    # add
     if (prompt == "1"):
         addEmployee()
-
+    # remove
     if (prompt == "2"):
         removeEmployee()
-
+    #update
     if (prompt == "3"):
         updateEmployee()
-
+    # view all employee information
     if (prompt == "4"):
         viewAll()
-
+    # view one employee information
     if (prompt == "5"):
         viewOne()
 promptUser()
